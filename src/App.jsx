20cd38973f9331b70cfb9c2844dbeeeb19adddfa
alex <more2write1code@gmail.com>
2026-05-01 14:02:04 +0200

    loaded main page img

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 export default function App() {
   return (
     <div className="h-screen flex items-center justify-center bg-black text-white text-4xl">
-      Tailwind работает 🚀
+      Alex Art
     </div>
   )
 }
